3f24922cb2bb10f2441170e78502233966ab61f5
danzongd <danzongd1024@gmail.com>
2026-02-21 20:12:31 +0800

    modified vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,18 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [tailwindcss()],
+    build: {
+        rollupOptions: {
+            input: {
+                // 配置所有的入口页面 (Entry points)
+                main: resolve(__dirname, "index.html"),
+                archives: resolve(__dirname, "archives.html"),
+                galerie: resolve(__dirname, "galerie.html"),
+                contact: resolve(__dirname, "contact.html"),
+            },
+        },
+    },
 });
